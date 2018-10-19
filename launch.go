05f2ec0d8a3a58b96e2c29894eafae903fbf23c2
Trip Dishman <tripdishman@gmail.com>
2018-10-18 22:36:41 -0500

    Switched start to isostart date

--- launch.go
+++ launch.go
@@ -1,10 +1,15 @@
 package main
 
+// Data types used when fetching
+// info from launchlibrary.net
+//
+//
+
 // Launch object for top level data
 type Launch struct {
 	ID    int    `json:"id"`
 	Name  string `json:"name"`
-	Start string `json:"windowstart"`
+	Start string `json:"isostart"`
 }
 
 // Launches array for loading
