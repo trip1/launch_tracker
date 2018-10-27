b4d5d80acd8481ac10513aa0724ca84c30ce20a0
Trip Dishman <tripdishman@gmail.com>
2018-10-27 02:27:54 -0500

    Added interval updates with manual update on request

--- web_server.go
+++ web_server.go
@@ -13,18 +13,25 @@ import (
 // Server handles HTTP traffic
 // and state management of data
 type Server struct {
-	Launches        Launches      `json:"launches"`
-	UpdatedAt       time.Time     `json:"updated_at"`
-	TimeSinceUpdate time.Duration `json:"time_since_update"`
+	Launches   Launches    `json:"launches"`
+	Ticker     time.Ticker `json:"ticker"`
+	UpdatedAt  time.Time   `json:"updated_at"`
+	Update     chan bool   `json:"update"`
+	StopUpdate chan bool   `json:"stop"`
 }
 
 func (s *Server) init() {
 	s.fetchLaunches()
+
+	s.Update = make(chan bool)
+	s.StopUpdate = make(chan bool)
+
 	fmt.Println("Init complete")
+	go s.interval()
 }
 
 func (s *Server) fetchLaunches() {
-	resp, err := http.Get("https://launchlibrary.net/1.4/launch/next/10")
+	resp, err := http.Get("https://launchlibrary.net/1.4/launch/next/10&mode=verbose")
 	if err != nil {
 		fmt.Println("Failed to get launch data")
 	}
@@ -33,15 +40,28 @@ func (s *Server) fetchLaunches() {
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
-	launches := Launches{}
 
 	// Read resp into array
-	err = json.Unmarshal(body, &launches)
+	err = json.Unmarshal(body, &s.Launches)
 	if err != nil {
 		fmt.Println(err)
 	}
+}
 
-	s.Launches = launches
+func (s *Server) interval() {
+	ticker := time.NewTicker(6 * time.Hour)
+
+	for {
+		select {
+		case <-s.Update:
+			fmt.Println("Update")
+			s.fetchLaunches()
+			s.UpdatedAt = time.Now()
+		case <-ticker.C:
+			s.fetchLaunches()
+			s.UpdatedAt = time.Now()
+		}
+	}
 }
 
 func (s *Server) home(ctx iris.Context) {
@@ -55,4 +75,9 @@ func (s *Server) home(ctx iris.Context) {
 
 func (s *Server) homeJSON(ctx iris.Context) {
 	ctx.JSON(s.Launches)
+}
+
+func (s *Server) update(ctx iris.Context) {
+	s.Update <- true
+	ctx.WriteString("Done")
 }
